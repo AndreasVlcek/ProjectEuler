8e756038246f2409b9bee66de64ff394d458fe9b
ANDREAS VLCEK <andreas.vlcek@googlemail.com>
2020-01-17 13:11:50 +0100

    Solution to problem #56
    Add solution to problem #56

---
+++ Problem 56/PowerfulDigitSum.java
@@ -0,0 +1,34 @@
+import java.math.BigInteger;
+
+
+public class PowerfulDigitSum {
+
+    public static void main(String[] args) {
+
+        long max = 0;
+
+        for (int i = 1; i < 100; i++) {
+            for (int j = 1; j < 100; j++) {
+
+
+                BigInteger num = new BigInteger(String.valueOf(i)).pow(j);
+
+                String numAsString = num.toString();
+
+                char[] stringArray = numAsString.toCharArray();
+
+                long sum = 0;
+                for (int k = 0; k < stringArray.length; k++) {
+                    sum = sum + stringArray[k] - '0'; // To get the value of any character digit, just remove '0' (48)
+                }
+
+                if (sum > max) {
+                    max = sum;
+                }
+
+            }
+        }
+
+        System.out.println(max);
+    }
+}
