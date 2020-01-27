5ef46ac698b1e26cf82ac988fa1256db80db3b01
ANDREAS VLCEK <andreas.vlcek@googlemail.com>
2020-01-27 10:01:22 +0100

    Solution Problem 57

---
+++ Problem 57 /SquareRootConvergents.java
@@ -0,0 +1,30 @@
+import java.math.BigInteger;
+
+public class SquareRootConvergents {
+
+
+    public static void main(String[] args) {
+
+        int count = 0;
+
+        BigInteger numerator = new BigInteger("3");
+        BigInteger denominator = new BigInteger("2");
+
+        for (int i = 1; i < 1000; i++) {
+
+            // This ia all about pattern matching
+            // numerator = numerator + 2 * denominator
+            // denominator = numerator - denominator
+            numerator = numerator.add(BigInteger.valueOf(2L).multiply(denominator));
+            denominator = numerator.subtract(denominator);
+
+            if (numerator.toString().length() > denominator.toString().length()) {
+                count++;
+            }
+
+        }
+
+        System.out.println(count);
+
+    }
+}
